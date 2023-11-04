c8d4c43d56d8628a7aece1aea4f0e712c867c7f3
ThaoLang <90896633+ThaoLang@users.noreply.github.com>
2023-11-04 19:09:06 +0700

    fix: fix load more

---
+++ public/env.js
@@ -0,0 +1,4 @@
+window.env = {
+  "BASE_URL": "https://api.unsplash.com/search/photos/?query=",
+  "API_KEY": "nIpxc6YZnCXpIY4PVmDKrsn-G8QWefYadyuRCJHWPoU"
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,31 +7,75 @@ import ProductList from '@/component/ProductList'
 import { useEffect, useState } from 'react';
 import axios from 'axios'
 import { productItemProp } from '@/component/ProductItem'
-import 'dotenv/config'
+import env from "react-dotenv";
 
 export default function Home() {
-  const API_KEY='J72Bm0svXRwDbJ3f6B0p2ZxLQ_qsxKVAOqR6RwBM7VQ'
+  const API_KEY='hmbMi7acz1ugLaMEVFPJgsbKphCGcRR8E0u8hprINL8'
   const BASE_URL='https://api.unsplash.com/search/photos/?query='
   const [itemList, setItemList] = useState<productItemProp[]>([]);
-  const [searchText,setSearchText] =useState("");
- 
+  const [searchText,setSearchText] =useState("blue");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const loadingNotification ="Loading...";
+  const [page, setPage] = useState(1);
+
   async function loadImage() {
+    setItemList([]);
     try {
-      const res = await axios.get(`${BASE_URL}${searchText}&client_id=${API_KEY}&per_page=100`);
-      //const res = await axios.get(`${process.env.BASE_URL}${searchText}&client_id=${process.env.API_KEY}&per_page=1000`);
-      console.log(res);
+      setIsLoading(true); 
+      const res = await axios.get(`${BASE_URL}${searchText}&client_id=${API_KEY}&per_page=10`);
       const productResult = await res.data.results;
       setItemList(productResult?.map((item: productItemProp) => ({ url: item.urls.small, description: item.description })))
     } catch (error) {
       console.error("Error loading data:", error);
     }
+    finally {
+    setIsLoading(false); 
+  }
   } 
-  
 
+  
+  
+  
   
   useEffect(() =>{
     loadImage();
+    setSearchText("");
   },[])
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        window.innerHeight + window.scrollY >= document.body.offsetHeight - 5 &&
+        !isLoading
+      ) {
+        if(!isLoadingMore){
+          setIsLoadingMore(true);
+        }
+      }
+    });
+  }, []);
+
+
+  useEffect(() =>{
+    if(!isLoadingMore){
+      return;
+    }
+    async function loadMoreImages() {
+      try {        
+        const res = await axios.get(`${BASE_URL}${searchText}&client_id=${API_KEY}&per_page=10&page=${page + 1}`);
+        const newImages = res.data.results;
+        setItemList((prevItems) => [...prevItems, ...newImages.map((item: productItemProp) => ({ url: item.urls.small, description: item.description }))]);
+        setPage((prevPage) => prevPage + 1);
+      } catch (error) {
+        console.error("Error loading more data:", error);
+      } finally {
+        setIsLoadingMore(false);
+      }
+    }
+    loadMoreImages()
+  },[isLoadingMore]);
+  
  
   return (
     <div className="relative">
@@ -39,8 +83,12 @@ export default function Home() {
       <SearchBar text={searchText} onChangeText={setSearchText} onClick={loadImage} />
     </div>
     <div className="mt-2 ">
-      <div className="relative">
-        <ProductList products={itemList} />
+      <div className="relative flex-col">
+      
+         <ProductList products={itemList} />
+         {(isLoading || isLoadingMore) && 
+            <label className='text-xl text-center justify-content'>{loadingNotification}</label>
+      }
       </div>
     </div>
   </div>
